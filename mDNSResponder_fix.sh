0898491176e7ad0239134b91d0c47325706f91fe
Brian J. Creasy <genetik@gmail.com>
2013-11-22 17:52:51 -0600

    allow for debugging using the included reference plist

--- mDNSResponder_fix.sh
+++ mDNSResponder_fix.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 plist=/System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+if [[ $1 == "--debug" ]]; then
+  debug=1
+  plist=plist/com.apple.mDNSResponder.plist
+fi
 
 sudo -l || exit # don't continue if sudo fails
 
 sudo sed -i .orig 's/\(<string>-launchd<\/string>\)/\1\
                 <string>-AlwaysAppendSearchDomains<\/string>/
 ' $plist
- 
-sudo launchctl unload -w $plist
- 
-sudo launchctl load -w $plist
+
+if [[ -z $debug ]]; then
+  echo sudo launchctl unload -w $plist
+  echo sudo launchctl load -w $plist
+fi
