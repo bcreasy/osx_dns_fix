f6e1cb392487bc6e4ea37459c11fbabb35e59651
Brian Creasy <bcreasy@starmount.com>
2014-07-28 11:44:31 -0500

    if in debug mode and mock plist doesn't exist for the version of osx you're running, exit

--- mDNSResponder_fix.sh
+++ mDNSResponder_fix.sh
@@ -12,6 +12,10 @@ kernel=$(uname -r)
 if [[ $1 == "--debug" ]]; then
   debug=1
   plist=plist/com.apple.mDNSResponder.plist.$kernel
+  if [ ! -e $plist ]; then
+    echo "Error: Mock plist not found for kernel version ${kernel}.  Create one based off of the default installation for this version of OSX before debugging can be used."
+    exit 1
+  fi
 fi
 
 sudo -l || exit # don't continue if sudo fails
